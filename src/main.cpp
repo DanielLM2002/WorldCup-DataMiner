c50245cb10f05caa11fcda8c2fd2f7bafbd677a9
estebancb03 <esteban.castaneda@ucr.ac.cr>
2022-08-30 11:25:30 -0600

    Se agrega el main y el .gitignore

---
+++ src/main.cpp
@@ -0,0 +1,43 @@
+/// @copyright 2022 ECCI, Universidad de Costa Rica. All rights reserved
+/// @author Esteban Castañeda Blanco <esteban.castaneda@ucr.ac.cr>
+/// This code is released under the GNU Public License version 3
+
+#define IPV4
+
+#include <stdio.h>
+#include <string.h>
+
+#include "Socket.hpp"
+
+#ifdef IPV4
+int main(int argc, char * argv[]) {
+  const char* os = "http://os.ecci.ucr.ac.cr/";
+  // const char* osn = "10.1.104.187";  // Public ip
+  const char* osn = "163.178.104.187";  // Private ip  
+  const char* request = "GET /futbol/2018/world-cup-2018.json HTTP/1.1\r\nhost: redes.ecci\r\n\r\n";
+  Socket s('s');
+  char a[512];
+  s.Connect(osn, 80);
+  s.Write(request);
+  while (s.Read(a, 512) != 0) {
+    printf("%s\n", a);
+    memset(a, 0, 512);
+  }
+}
+#endif
+
+#ifdef IPV6
+int main( int argc, char * argv[] ) {
+  const char* os = "fe80::194d:4e78:60c:fedc%eno1";
+  const char* request = "GET / HTTP/1.1\r\nhost: redes.ecci\r\n\r\n";
+
+  Socket s('s', true);
+  char a[512];
+
+  memset(a, 0, 512);
+  s.Connect(os, (char *) "http");
+  s.Write(request);
+  s.Read(a, 512);
+  printf("%s\n", a);
+}
+#endif
