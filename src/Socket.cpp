09fff80c8277d319d615218e7c3a5b21f140cdd6
estebancb03 <esteban.castaneda@ucr.ac.cr>
2022-08-30 11:22:02 -0600

    Se actualiza el Socket.cpp

--- src/Socket.cpp
+++ src/Socket.cpp
@@ -1,108 +1,76 @@
-#include "Socket.h"
+/// @copyright 2022 ECCI, Universidad de Costa Rica. All rights reserved
+/// @author Esteban Castañeda Blanco <esteban.castaneda@ucr.ac.cr>
+/// This code is released under the GNU Public License version 3
 
+#include "Socket.hpp"
 
-/* 
-   char tipo: el tipo de socket que quiere definir
-      's' para "stream
-      'd' para "datagram"
-   bool ipv6: si queremos un socket para IPv6
+/**
+ * @brief Socket constructor
+ * @param type Specifies the communication semantics
+ * @param ipv6 Indicates if ipv6 is used
  */
-Socket::Socket( char type, bool ipv6 ){
-   this -> port = default_port;
-   this -> ipv6 = ipv6;
-   this -> idSocket = -1;
-   if (!this -> ipv6) {
-      this -> idSocket = type == 's' ? socket(AF_INET, SOCK_STREAM, 0)
-      :socket(AF_INET, SOCK_DGRAM, 0);
-   } else {
-      this -> idSocket = type == 's' ? socket(AF_INET6, SOCK_STREAM,0)
-      :socket(AF_INET6, SOCK_DGRAM, 0);
-   }
+Socket::Socket(char type, bool ipv6) {
+  this->ipv6 = ipv6;
+  this->id = -1;
+  this->port = DEFAULT_PORT;
+  if (!this->ipv6)
+    this->id = type == 's' ? socket(AF_INET, SOCK_STREAM, 0)
+                           : socket(AF_INET, SOCK_DGRAM, 0);
+  else
+    this->id = type == 's' ? socket(AF_INET6, SOCK_STREAM, 0)
+                           : socket(AF_INET6, SOCK_DGRAM, 0);
 }
 
-Socket::~Socket(){
-   this->Close();
-}
-
-
-void Socket::Close(){
-   close(this->idSocket);
-
-}
-
-/*
-   char * hostip: direccion del servidor, por ejemplo "www.ecci.ucr.ac.cr"
-   int port: ubicacion del proceso, por ejemplo 80
+/**
+ * @brief Socket destroyer
  */
-int Socket::Connect( const char * hostip, int port ) {
-   struct sockaddr_in  host4;
-   memset(reinterpret_cast <char *>(&host4), 0, sizeof(host4));
-   host4.sin_family = AF_INET;
-   inet_pton(AF_INET, hostip, &host4.sin_addr );
-   host4.sin_port = htons( port );
-   int st = connect(this->idSocket, reinterpret_cast<sockaddr *>(&host4), sizeof(host4));
-   if ( -1 == st ) {
-      perror( "Socket::Connect" );
-      exit( 2 );
-   }
-   return st;
+Socket::~Socket() {
+  this->Close();
 }
 
-
-/*
-   char * hostip: direccion del servidor, por ejemplo "www.ecci.ucr.ac.cr"
-   char * service: nombre del servicio que queremos acceder, por ejemplo "http"
+/**
+ * @brief Method that connects with a port and an ip address
+ * @param hostip Ip address
+ * @param port Port number
+ * @return int 
  */
-int Socket::Connect( const char *host, const char *service ) {
-
-   return -1;
-
+int Socket::Connect(const char* hostip, int port) {
+  struct sockaddr_in host4;
+  memset(reinterpret_cast<char *>(&host4), 0, sizeof(host4));
+  host4.sin_family = AF_INET;
+  inet_pton(AF_INET, hostip, &host4.sin_addr);
+  host4.sin_port = htons(port);
+  int st = connect(this->id, reinterpret_cast<sockaddr *>(&host4),
+    sizeof(host4));
+  if (st == -1) {
+    perror("Socket::Connect");
+    exit(2);
+  }
+  return st;
 }
 
-
-int Socket::Read( char* text, int len ) {
-   return read(this->idSocket, text, len);
+/**
+ * @brief Method that reads from a socket
+ * @param info Information to read
+ * @param length Size of the information vector
+ * @return int 
+ */
+int Socket::Read(char* info, int length) {
+  return read(this->id, info, length);
 }
 
-
-int Socket::Write( const char* text ) {
-   return write(this->idSocket, text, strlen(text));
+/**
+ * @brief Method that writes to a socket
+ * @param info Information to write
+ * @return int 
+ */
+int Socket::Write(const char* info) {
+  return write(this->id, info, strlen(info));
 }
 
-
-int Socket::Listen( int queue ) {
-
-   if(listen(this->idSocket,queue)==-1){
-      std::cerr<<strerror(errno)<<std::endl;
-   }
-   return errno;
-
-}
-
-
-int Socket::Bind( int port ) {
-
-   return -1;
-
-}
-
-
-Socket * Socket::Accept(){
-
-   return (Socket *) 0;
-
-}
-
-
-int Socket::Shutdown( int mode ) {
-
-   return -1;
-
-}
-
-
-void Socket::SetIDSocket(int id){
-
-   idSocket = id;
-
+/**
+ * @brief Method that closes a socket
+ */
+void Socket::Close() {
+  close(this->id);
 }
