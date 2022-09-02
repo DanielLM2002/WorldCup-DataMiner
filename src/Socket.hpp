8ba9acb3e22c783c2dbec787fda8adcf8b99699c
Daniel Lizano <dlizano@outlook.com>
2022-09-02 10:19:58 -0600

    se comentan metodos no implementados

--- src/Socket.hpp
+++ src/Socket.hpp
@@ -35,16 +35,16 @@ class Socket {
   int Write(const char*, int);
   int Write(const char*);
   int Listen(int);
-  int Bind(int);
+  //int Bind(int);
   Socket* Accept();
   int Shutdown(int);
-  void SetId(int);
+  //void SetId(int);
   void InitSSLContext();
 	void InitSSL();
-	int SSLConnect( char *, int );
-	int SSLConnect( char *, char * );
-	int SSLRead( void *, int );
-	int SSLWrite( void *, int );
+	int SSLConnect(char *, int);
+	int SSLConnect(char *, char *);
+	int SSLRead(void *, int);
+	int SSLWrite(void *, int);
 };
 
 #endif  // !SOCKET_HPP
