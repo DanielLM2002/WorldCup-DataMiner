3f53b7c21da53e05b27e2b7e1bf654f8924f2cd2
estebancb03 <esteban.castaneda@ucr.ac.cr>
2022-09-02 15:08:32 -0600

    Ordenamiento e indentación

--- src/Socket.cpp
+++ src/Socket.cpp
@@ -25,22 +25,17 @@ Socket::Socket( int id ) {
   this->id = id;
 }
 
-
 Socket::~Socket() {
   this->Close();
-
-// SSL destroy
-   if ( nullptr != this->SSLContext ) {
-      SSL_CTX_free( (SSL_CTX *) this->SSLContext );
-   }
-   if ( nullptr != this->SSLStruct ) {
-      SSL_free( (SSL *) this->SSLStruct );
-   }
-
+  // SSL destroy
+  if (this->SSLContext == nullptr)
+    SSL_CTX_free( (SSL_CTX *) this->SSLContext );
+  if (this->SSLStruct == nullptr)
+    SSL_free( (SSL *) this->SSLStruct );
 }
 
 void Socket::Close(){
-  close( this->id );
+  close(this->id);
 }
 
 int Socket::Connect(const char* hostip, int port) {
@@ -57,120 +52,82 @@ int Socket::Connect(const char* hostip, int port) {
   }
   return st;
 }
+
 int Socket::Connect(const char* hostip, const char* port) {
-   struct sockaddr_in host4;
-   memset(reinterpret_cast<char *>(&host4), 0, sizeof(host4));
-   host4.sin_family = AF_INET;
-   inet_pton(AF_INET, hostip, &host4.sin_addr);
-   host4.sin_port = htons(atoi(port));
-   int st = connect(this->id, reinterpret_cast<sockaddr *>(&host4),
-      sizeof(host4));
-   if (st == -1) {
-      perror("Socket::Connect");
-      exit(2);
-   }
-   return st;
+  struct sockaddr_in host4;
+  memset(reinterpret_cast<char *>(&host4), 0, sizeof(host4));
+  host4.sin_family = AF_INET;
+  inet_pton(AF_INET, hostip, &host4.sin_addr);
+  host4.sin_port = htons(atoi(port));
+  int st = connect(this->id, reinterpret_cast<sockaddr *>(&host4),
+           sizeof(host4));
+  if (st == -1) {
+    perror("Socket::Connect");
+    exit(2);
+  }
+  return st;
 }
-// SSL methods
 
-/*
- *
- */
 void Socket::InitSSLContext() {
-   const SSL_METHOD * method = TLS_client_method();
-   SSL_CTX * context = SSL_CTX_new( method );
-
-   if ( nullptr == context ) {
-        perror( "Socket::InitSSLContext" );
-        exit( 23 );
-    }
-
-    this->SSLContext = (void *) context;
-
+  const SSL_METHOD* method = TLS_client_method();
+  SSL_CTX* context = SSL_CTX_new(method);
+  if (context == nullptr) {
+    perror( "Socket::InitSSLContext" );
+    exit( 23 );
+  }
+  this->SSLContext = (void *) context;
 }
 
-
-/*
- *
- */
 void Socket::InitSSL() {
-
-   this->InitSSLContext();
-   SSL * ssl = SSL_new( ((SSL_CTX *) this->SSLContext ) );
-   if ( nullptr == ssl ) {
-      perror( "Socket::InitSSL" );
-      exit( 23 );
-   }
-   this->SSLStruct = (void *) ssl;
-
+  this->InitSSLContext();
+  SSL* ssl = SSL_new(((SSL_CTX *) this->SSLContext));
+  if (ssl == nullptr) {
+    perror( "Socket::InitSSL" );
+    exit( 23 );
+  }
+  this->SSLStruct = (void *) ssl;
 }
 
-
-/*
- *
- */
-int Socket::SSLConnect( const char * host, int port ) {
-   int resultado;
-
-   this->Connect( host, port );	// Establish a non ssl connection first
-   SSL_set_fd( (SSL *) this->SSLStruct, this->id );
-   resultado = SSL_connect( (SSL *) this->SSLStruct );
-   if ( -1 == resultado ) {
-      perror( "Socket::SSLConnect" );
-      exit( 23 );
-   }
-  return resultado;
+int Socket::SSLConnect(const char* host, int port) {
+  int result;
+  this->Connect( host, port );  // Establish a non ssl connection first
+  SSL_set_fd((SSL *) this->SSLStruct, this->id);
+  result = SSL_connect((SSL *) this->SSLStruct);
+  if (result == -1) {
+    perror( "Socket::SSLConnect" );
+    exit( 23 );
+  }
+  return result;
 }
 
-
-/*
- *
- */
-int Socket::SSLConnect( const char * host, char * service ) {
-   int resultado;
-
-   this->Connect( host, service );
-   SSL_set_fd( (SSL *) this->SSLStruct, this->id );
-   resultado = SSL_connect( (SSL *) this->SSLStruct );
-   if ( -1 == resultado ) {
-      perror( "Socket::SSLConnect" );
-      exit( 23 );
-   }
-  return resultado;
-
+int Socket::SSLConnect(const char* host, char* service) {
+  int result;
+  this->Connect(host, service);
+  SSL_set_fd((SSL *) this->SSLStruct, this->id);
+  result = SSL_connect((SSL *) this->SSLStruct);
+  if (result == -1) {
+    perror( "Socket::SSLConnect" );
+    exit( 23 );
+  }
+  return result;
 }
 
-
-/*
- *
- */
-int Socket::SSLRead( void * buffer, int size ) {
-   int resultado;
-
-   resultado = SSL_read( (SSL *) this->SSLStruct, buffer, size );
-   if ( -1 == resultado ) {
-      perror( "Socket::SSLRead" );
-      exit( 23 );
-   }
-
-   return resultado;
-
+int Socket::SSLRead(void* buffer, int size) {
+  int result;
+  result = SSL_read((SSL *) this->SSLStruct, buffer, size);
+  if (result == -1) {
+    perror( "Socket::SSLRead" );
+    exit( 23 );
+  }
+  return result;
 }
 
-
-/*
- *
- */
-int Socket::SSLWrite( void* buffer, int size ) {
-   int resultado;
-
-   resultado = SSL_write( (SSL *) this->SSLStruct, buffer, size );
-   if ( -1 == resultado ) {
-      perror( "Socket::SSLWrite" );
-      exit( 23 );
-   }
-
-   return resultado;
-
+int Socket::SSLWrite(void* buffer, int size) {
+  int result;
+  result = SSL_write((SSL *) this->SSLStruct, buffer, size);
+  if ( result == -1) {
+    perror( "Socket::SSLWrite" );
+    exit( 23 );
+  }
+  return result;
 }
-
