aae93c361c7eac020961b46be8ed7c8220f28b2e
estebancb03 <esteban.castaneda@ucr.ac.cr>
2022-11-19 15:54:40 -0600

    Se agrega cmetodo que llena la tabla de grupos con datos de un archivo externo

--- src/Router/Router.cpp
+++ src/Router/Router.cpp
@@ -15,4 +15,27 @@ Router::~Router() {
 
 }
 
+void Router::fillGroupsTable(std::string fileName) {
+  json groups;
+  json jsonData;
+  int firstChar;
+  std::ifstream file;
+  std::string line = "";
+  std::string data = "";
 
+  file.open(fileName, std::ios::in);
+  while (getline(file, line)) 
+    data += line;
+  // remove http headers
+  firstChar = data.find("{",0);
+  data = data.erase(0, firstChar);
+  jsonData = json::parse(data);
+  groups = jsonData["groups"];
+  for (json::iterator it = groups.begin(); it != groups.end(); ++it) {
+    std::string group = it.key();
+    std::vector<std::string> countries = it.value()["countries"]; 
+    // Save the data in the table
+    this->groupsTable[group] = countries;
+  }
+  file.close();
+}
