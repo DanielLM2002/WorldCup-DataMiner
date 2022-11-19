46946330e433fe565d72dfe122714b24561684b5
estebancb03 <esteban.castaneda@ucr.ac.cr>
2022-11-19 15:53:09 -0600

    Se agrega constructor y destructor de la clase Router

---
+++ src/Router/Router.cpp
@@ -0,0 +1,18 @@
+/// @copyright 2022 ECCI, Universidad de Costa Rica. All rights reserved
+/// @author Michelle Fonseca Carrillo <michelle.fonsecacarrillo@ucr.ac.cr>
+/// @author Esteban Castañeda Blanco <esteban.castaneda@ucr.ac.cr>
+/// @author Daniel Lizano Morales <daniel.lizanomorales@ucr.ac.cr>
+/// @author Jorge Loría López <jorge.lorialopez@ucr.ac.cr>
+/// This code is released under the GNU Public License version 3
+
+#include "Router.hpp"
+
+Router::Router() {
+
+}
+
+Router::~Router() {
+
+}
+
+
