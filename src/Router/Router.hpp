db3864680bc1a43e9b40ba73658892d19d4edc7d
estebancb03 <esteban.castaneda@ucr.ac.cr>
2022-11-19 16:26:26 -0600

    Documentacion

--- src/Router/Router.hpp
+++ src/Router/Router.hpp
@@ -18,19 +18,73 @@ using json = nlohmann::json;
 
 class Router {
  private:
+  /// Table that associates a server to a group
   std::map<std::string, char> serverTable;
+  /// Table that associates a group to an array of countries
   std::map<std::string, std::vector<std::string>> groupsTable;
 
  public:
+  /**
+   * @brief Constructor
+   */
   explicit Router();
+
+  /**
+   * @brief Destroyer
+   */
   ~Router();
+
+  /**
+   * @brief Method that fills the group table with the data from a JSON
+   * @param fileName File path
+   */
   void fillGroupsTable(std::string);
+
+  /**
+   * @brief Method that adds a server associated to a group to the 
+   *        servers table
+   * @param address IP address of the server
+   * @param group Group letter
+   */
   void addServer(std::string, char);
+
+  /**
+   * @brief Method that removes a server associated to a group to the 
+   *        servers table
+   * @param addres IP address of the server
+   */
   void removeServer(std::string);
+
+  /**
+   * @brief 
+   * 
+   */
   void listenForClients();
+
+  /**
+   * @brief 
+   * 
+   */
   void listenForServers();
+
+  /**
+   * @brief 
+   * 
+   */
   void sendWakeUpBroadcast();
+
+  /**
+   * @brief Method that gets the group associated with a server
+   * @param address IP address of the server
+   * @return char 
+   */
   char getGroupByServer(std::string);
+
+  /**
+   * @brief Method that gets the server associated with a group
+   * @param group Group letter
+   * @return std::string 
+   */
   std::string getServerByGroup(char);
 };
 
